0326caebe42f839d6f7a9d70bf25886ab90dd81e
Efotsowoe <105896538+Efotsowoe@users.noreply.github.com>
2022-07-01 21:26:34 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.17.180:7000/',
+  serverUrl: 'http://54.221.36.11/:7000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
